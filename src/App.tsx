79b4d47212b2d96a2ef7d1eac31abc77d85f9f38
Agustin Carnevale <acarnevale@ecointeractive.com>
2021-03-24 10:00:45 -0300

    added multi environments for webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './styles.css';
 import Image from '../assets/colors.png';
-import PineappleSVG from '../assets/Pineapple.svg';
+import PineappleSVG from '../assets/pineapple.svg';
 
 const App = () => {
     return <div>
-        <h1>React Typescript WebPack Template</h1>
+        <h1>React Typescript WebPack Template - {process.env.NODE_ENV} - {process.env.name}</h1>
         <img src={Image} alt="Colors" width="300" height="400" />
         <img src={PineappleSVG} alt="Pineapple" width="150"/>
     </div> 
