4fa7d92e09c671548a3a6ace78b91dea16d28aac
Agustin Carnevale <acarnevale@ecointeractive.com>
2021-03-31 11:41:29 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Image from '../assets/colors.png'
 import PineappleSVG from '../assets/pineapple.svg'
 
 const App = () => {
-
+  const name = 'Agustin'
   return (
     <div>
       <h1>
         React Typescript WebPack Template - {process.env.NODE_ENV} -{' '}
         {process.env.name}
+        {name}
       </h1>
       <img src={Image} alt="Colors" width="300" height="400" />
       <img src={PineappleSVG} alt="Pineapple" width="150" />
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 
-const num = 0
-
 ReactDOM.render(<App />, document.getElementById('root'))
