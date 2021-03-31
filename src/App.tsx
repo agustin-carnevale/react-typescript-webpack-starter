f4697bdcc70875ec49669f1545782c5ae83d2376
Agustin Carnevale <acarnevale@ecointeractive.com>
2021-03-31 10:54:15 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import './styles.css';
-import Image from '../assets/colors.png';
-import PineappleSVG from '../assets/pineapple.svg';
+import './styles.css'
+import Image from '../assets/colors.png'
+import PineappleSVG from '../assets/pineapple.svg'
 
 const App = () => {
-    return <div>
-        <h1>React Typescript WebPack Template - {process.env.NODE_ENV} - {process.env.name}</h1>
-        <img src={Image} alt="Colors" width="300" height="400" />
-        <img src={PineappleSVG} alt="Pineapple" width="150"/>
-    </div> 
+
+  return (
+    <div>
+      <h1>
+        React Typescript WebPack Template - {process.env.NODE_ENV} -{' '}
+        {process.env.name}
+      </h1>
+      <img src={Image} alt="Colors" width="300" height="400" />
+      <img src={PineappleSVG} alt="Pineapple" width="150" />
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const num = 0
+
+ReactDOM.render(<App />, document.getElementById('root'))
